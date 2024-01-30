01b00ce5a4950f8353532799e6075c88a483fbb5
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2024-01-29 17:48:01 -0800

    adding cdf plots

--- story_eval/visualize.py
+++ story_eval/visualize.py
@@ -15,18 +15,7 @@ def add_radar_chart_filled_variance(ax, angles, data_row, label, color, variance
     ax.plot(angles, lower_values, color=color, linewidth=0)
     ax.fill_between(angles, lower_values, upper_values, color=color, alpha=0.1)
 
-def plot_spider(df, models, title, save_path=None):
-  
-    sort_order = {
-        "Llama-2-7B" : 0, 
-        "Llama-2-13B": 1, 
-        "Vicuna-33B" : 2,
-        "Llama-2-70B": 3,
-        "GPT-4":       4,
-        "Human-Low":   5,
-        "Human-Mid":   6,
-        "Human-High":  7,
-    }
+def plot_spider(df, models, title, save_path=None, sort_order=None):
 
     # Filter data for specific models
     filtered_data = df[df["model_short"].isin(models)]
@@ -68,7 +57,54 @@ def plot_spider(df, models, title, save_path=None):
     else:
         plt.show()
 
+def plot_model_cdf(data, model, column, ax, color):
+    model_data = data[data['model_short'] == model]
+    values, base = np.histogram(model_data[column], bins=np.arange(1, 7), density=True)
+    cumulative = np.cumsum(values)
+    
+    ax.plot(base[:-1], cumulative, marker='o', label=model, color=color)
+    ax.set_title(f'{column.replace("_", " ").title()}')
+    ax.set_xlabel('Scores')
+    ax.set_ylabel('Cumulative Probability')
+    ax.set_xticks(np.arange(1, 6))
+    ax.set_ylim([0, 1])
 
+def plot_cdf(df, models, title, nrows, ncols, save_path, sort_order):
+
+    # Filter data for specific models
+    filtered_data = df[df["model_short"].isin(models)]
+
+    # Sorting the unique models based on the provided order
+    sorted_df = filtered_data.sort_values(by=["model_short"], key=lambda x: x.map(sort_order))
+
+    # Prepare data and labels for the radar chart
+    score_columns = [col for col in sorted_df.columns if col.endswith('_score')]
+    colors = plt.cm.magma(np.linspace(0, 1, len(models)))
+
+    # Creating the updated plots
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5 * ncols, 5 * nrows), sharex='col', sharey='row')
+    fig.suptitle(title, fontsize=16)
+
+    # Flatten axes array if necessary
+    if nrows * ncols > 1:
+        axes = axes.flatten()
+    else:
+        axes = [axes]
+
+    # Plotting the CDF for each component, including all models in the specified order
+    for i, column in enumerate(score_columns):
+        for model, color in zip(models, colors):
+            plot_model_cdf(sorted_df, model, column, axes[i], color)
+
+    # Adjust layout and add a combined legend
+    plt.tight_layout(rect=[0, 0.03, 1, 0.96])
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=4, fontsize='large')
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches='tight')
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -91,21 +127,36 @@ if __name__ == "__main__":
 
     save_path = "./story_eval/imgs/"
 
+    # Spider Plots
     plot_spider(
         df=human_ratings_df, 
         models=list(sort_order)[4:], 
         title="GPT-4 vs Human Quaility Levels", 
-        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_humans.png")
+        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_humans.png"),
+        sort_order=sort_order
     )
     plot_spider(
         df=human_ratings_df, 
         models=list(sort_order)[:5], 
         title="GPT-4 vs Other LLMs", 
-        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_llms.png")
+        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_llms.png"),
+        sort_order=sort_order
     )
     plot_spider(
         df=human_ratings_df, 
         models=list(sort_order), 
         title="All Models", 
-        save_path=os.path.join(save_path, "mean_scores_all.png")
+        save_path=os.path.join(save_path, "mean_scores_all.png"),
+        sort_order=sort_order
+    )
+
+    # CDF plots
+    plot_cdf(
+        df=human_ratings_df, 
+        models=list(sort_order), 
+        nrows=2, 
+        ncols=3,
+        title="CDF for Each Psychological Depth Component Across Models", 
+        save_path=os.path.join(save_path, "cdf_all.png"),
+        sort_order=sort_order
     )
