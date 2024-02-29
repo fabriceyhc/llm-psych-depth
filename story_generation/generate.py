d418c7a7c74bed1e307433232da5d0b7163c680f
Alexia Zhou <alexiazhou726@ucla.edu>
2024-02-28 20:46:52 -0800

    add reddit premises; update writer profile generation framework

--- story_generation/generate.py
+++ story_generation/generate.py
@@ -1,57 +1,28 @@
-from utils import *
-from loader import DataLoader
-from writer_profile import WriterProfilePromptsGenerator
+import pandas as pd
+from tqdm import tqdm
+
+from writer_profile import ZeroShotWriterProfileGenerator
 from plan_write import PlanWritePromptsGenerator
-from custom_llm import CustomLLM
-from bard_llm import BardLLM
-
-
-from langchain.llms import OpenAI
-
-def main():
-
-    llm = CustomLLM()
-    # llm = OpenAI(openai_api_key="xxx", model_name="gpt-4")
-    # llm = BardLLM()
-
-    writer_profile_generator = WriterProfilePromptsGenerator(llm=llm)
-    plan_write_generator = PlanWritePromptsGenerator(llm=llm)
-
-    loader = DataLoader()
-    r_wp_dir = "../data/v3/"
-    r_wp_df = loader.load_reddit_df(r_wp_dir, sort_by='prompt')
-    r_prompts = r_wp_df['prompt']
-
-    save_dir = "../llm_story_generation_results_v1_regen/"
-    model_name = "bard-2023.10.30"
-
-    num_generations = 3
-    # To generate stories for all, set regen_ids to empty or None 
-    writer_profile_regen_ids = [0, 1, 2]
-    plan_write_regen_ids = [3, 4, 5]
-
-    for n in range(num_generations):
-        writer_profile_generator.prompt_llm(r_prompts, save_dir, model_name, writer_profile_regen_ids)
-        plan_write_generator.prompt_llm(r_prompts, save_dir, model_name, plan_write_regen_ids)
-
-    # print('### Writer Profile Story Generation Example Prompt ###\n' +
-    #       '-' * 54 + '\n' +
-    #       writer_profile_generator.generate_prompts(r_prompts)[0]['story_generation_prompt'] + '\n' +
-    #       '=' * 54 + '\n')
-
-    # print('### Plan + Write Characters Generation Example Prompt ###\n' +
-    #       '-' * 57 + '\n' +
-    #       plan_write_generator.generate_character_prompts(r_prompts)[0]['characters_prompt'] + '\n' +
-    #       '=' * 57 + '\n')
-    
-    # planwrite_dir = "../gpt4_story_generation_results/plan_write_v2/"
-    # planwrite_df = loader.load_planwrite_df(planwrite_dir, sort_by='id')
-
-    # print('### Plan + Write Story Generation Example Prompt ###\n' +
-    #       '-' * 52 + '\n' +
-    #       plan_write_generator.generate_story_prompts(planwrite_df)[0]['story_prompt'] + '\n' +
-    #       '=' * 52 + '\n')
 
 
 if __name__ == '__main__':
-    main()
+
+    llm = "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ"
+
+    writer_profile_generator = ZeroShotWriterProfileGenerator(model_name_or_path=llm)
+
+    premises = pd.read_csv("../data/premises.csv")
+
+    save_dir = "../llm_story_generation_results_v2/"
+
+    # the number of stories to be generated per prompt
+    n_gen = 3
+    
+    # To generate stories for all, set regen_ids to empty or None 
+    wp_regen_ids = [0, 1, 2]
+    pw_regen_ids = [3, 4, 5]
+
+    # min. story length
+    min_len = 400
+
+    writer_profile_generator.output_stories(premises, save_dir, llm, n_gen, wp_regen_ids, min_len)
--- story_generation/writer_profile.py
+++ story_generation/writer_profile.py
@@ -1,106 +1,192 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["WORLD_SIZE"] = "1"
+
+import datetime
+import traceback
+import textwrap
+from tqdm import tqdm
+import pandas as pd
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.prompts.chat import ChatPromptTemplate
-from langchain.prompts import PromptTemplate
-from langchain.schema import BaseOutputParser, StrOutputParser
-from langchain.chains import LLMChain
-
-from utils import *
 
 
-writer_profile =\
-"""
-You are a seasoned writer who has won several accolades for your emotionally rich stories.
-When you write, you delve deep into the human psyche, pulling from the reservoir of universal experiences that every reader, regardless of their background, can connect to.
-Your writing is renowned for painting vivid emotional landscapes, making readers not just observe but truly feel the world of your characters.
-Every piece you produce aims to draw readers in, encouraging them to reflect on their own lives and emotions.
-Your stories are a complex tapestry of relationships, emotions, and conflicts, each more intricate than the last.
-"""
+class WriterProfileGenerator:
 
-story_prompt =\
-"""
-Now write a 500-word story on the following prompt:
+    def __init__(self, model_name_or_path="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ", revision="main",
+                 max_new_tokens=512, do_sample=True, temperature=0.7, top_p=0.95, 
+                 top_k=40, repetition_penalty=1.1, cache_dir="./.cache/",
+                 num_retries=10, use_system_profile=True):
+
+        self.model_name_or_path = model_name_or_path
+        self.use_system_profile = use_system_profile
+        self.num_retries = num_retries
+        self.strategy = "writer_profile"
+
+         # Initialize and load the model and tokenizer
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path,
+            cache_dir=cache_dir,
+            device_map="auto",
+            trust_remote_code=False,
+            revision=revision
+        ) 
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, use_fast=True, cache_dir=cache_dir
+        )
+
+        # Store the pipeline configuration
+        self.pipeline_config = {
+            "model": self.model,
+            "tokenizer": self.tokenizer,
+            "max_new_tokens": max_new_tokens,
+            "do_sample": do_sample,
+            "temperature": temperature,
+            "top_p": top_p,
+            "top_k": top_k,
+            "repetition_penalty": repetition_penalty
+        }
+
+        # Create the pipeline
+        pipe = pipeline("text-generation", **self.pipeline_config)
+        self.pipe = HuggingFacePipeline(pipeline=pipe)
+
+        # Define the output parser
+        self.output_parser = StrOutputParser()
+
+
+    def prompt_llm(self, premise, min_len=400, **kwargs):
     
-{prompt}
-
-Only respond with the story.
-"""
-
-
-class WriterProfilePromptsGenerator:
-
-    def __init__(self, llm) -> None:
-        self.llm = llm
-
-
-    class OutputParser(BaseOutputParser):
-        def parse(self, text: str):
-            return text
-
-
-    def generate_prompts(self, prompts):
-
-        prompts_to_run = []
-
-        for prompt_id, prompt in enumerate(prompts):
-            system_profile_prompt = SystemMessagePromptTemplate.from_template(writer_profile)
-            human_message_prompt = HumanMessagePromptTemplate.from_template(story_prompt)
-            chat_prompt = ChatPromptTemplate(
-                messages=[system_profile_prompt, human_message_prompt],
-                output_parser=self.OutputParser(),
-            )
-            _input = chat_prompt.format_messages(prompt=prompt)
-            prompts_to_run.append({
-                "prompt_id": prompt_id,
-                "reddit_prompt": prompt,
-                "story_generation_prompt": extract_string_prompt(_input)
-            })
-
-        return prompts_to_run
+        retry_count = 0
+        while retry_count < self.num_retries:
+            try:
+                dict_input = {"premise": premise}
+                output = self.chain.invoke(dict_input)
+                story_len = len(output.split())
+                if story_len < min_len:
+                    retry_count += 1
+                    print(f"Generated {story_len} (< {min_len}) words. Reprompting...")
+                    continue
+                dict_output = output.model_dump()
+                dict_output.update({
+                    "premise": premise,
+                    **kwargs,
+                    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                })
+                return dict_output
+            except Exception:
+                retry_count += 1
+                print(f"Failed to produce a valid story, trying again...")
+                if retry_count >= self.num_retries:
+                    print(f"Failed to produce a valid story after {retry_count} tries.")
+                    print(traceback.format_exc())
 
 
-    def prompt_llm(self, prompts, save_dir, model_name, regen_ids=None, template_type='writer_profile'):
+    def output_stories(self, premises, save_dir, llm, n_gen=3, regen_ids=None, min_len=400):
 
-        save_path = os.path.join(save_dir, model_name, template_type)
+        model_name = llm.split('/')[-1]
+        save_path = os.path.join(save_dir, f"{model_name}.csv")
         os.makedirs(save_path, exist_ok=True)
 
-        chat_prompt = ChatPromptTemplate.from_messages([
-            ("system", writer_profile),
-            ("human", story_prompt),
-        ])
-        chain = chat_prompt | self.llm | self.OutputParser()
+        stories = pd.DataFrame()
+        story_id = 0
 
-        indexed_prompts = [(id, prompt) for id, prompt in enumerate(prompts)]
+        for n_row, input_ in tqdm(premises.iterrows(), total=premises.shape[0]):
+        
+            if regen_ids and input_['premise_id'] not in regen_ids:
+                continue
 
-        if not regen_ids:
-            indexed_prompts = [(i, prompt) for i, prompt in indexed_prompts if i in regen_ids]
+            for i in range(n_gen):
 
-        for id, prompt in indexed_prompts:
+                response = self.prompt_llm(
+                    premise=input_['premise'],
+                    min_len=min_len,
+                    premise_id=input_['premise_id'],
+                    story_id=story_id,
+                    model_name=model_name,
+                    strategy=self.strategy,
+                    author_type="LLM",
+                )
 
-            max_tries = 3
-            min_words = 100
-            num_words, tries = 0, 0
-            while num_words < min_words and tries < max_tries:
-                output = chain.invoke({'prompt': prompt})
-                num_words = len(output.split())
-                if num_words < min_words:
-                    tries += 1
-                    print(f"Generated {num_words} words, fewer than {min_words} words. Trying {max_tries-tries} more times")
-            print(id)
-            print("-" * 20)
-            print(output)
-            print("=" * 50)
+                stories = stories._append(response, ignore_index=True)
+                story_id += 1
+        
+        stories.to_csv(save_path, index=False)
+        print(f"Stories saved to {save_path}")
 
-            save_info = {
-                "id": id,
-                "model_name": model_name,
-                "story_prompt": prompt,
-                "output": output
-            }
 
-            filename = f"{save_info['id']}_{first_n_words(save_info['story_prompt'])}_{generate_random_id()}.json"
-            with open(os.path.join(save_path, filename), 'w') as f:
-                json.dump(save_info, f, indent=4)
+class ZeroShotWriterProfileGenerator(WriterProfileGenerator):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+        # Define prompts
+        self.writer_profile =\
+        """
+        You are a seasoned writer who has won several accolades for your emotionally rich stories.
+        When you write, you delve deep into the human psyche, pulling from the reservoir of universal experiences that every reader, regardless of their background, can connect to.
+        Your writing is renowned for painting vivid emotional landscapes, making readers not just observe but truly feel the world of your characters.
+        Every piece you produce aims to draw readers in, encouraging them to reflect on their own lives and emotions.
+        Your stories are a complex tapestry of relationships, emotions, and conflicts, each more intricate than the last.
+        """
+
+        self.story_prompt =\
+        """
+        Now write a 500-word story on the following prompt:
+            
+        {premise}
+
+        Only respond with the story.
+        """
+
+        self.system_prompt = PromptTemplate(
+            template=self.writer_profile,
+        )
+
+        self.user_prompt = PromptTemplate(
+            template=self.story_prompt,
+            input_variables=["premise"],
+            template_format='jinja2',
+        )
+        
+        # Format prompt
+        if self.use_system_profile:
+            system_prompt = SystemMessagePromptTemplate(prompt=self.system_prompt)
+            user_prompt   = HumanMessagePromptTemplate(prompt=self.user_prompt)
+            self.prompt   = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
+        else:
+            self.prompt   = self.user_prompt
+
+        self.chain = self.prompt | self.pipe | self.output_parser
+
+
+if __name__ == '__main__':
+
+    llm = "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ"
+
+    generator = ZeroShotWriterProfileGenerator(model_name_or_path=llm)
+
+    premises = pd.read_csv("../data/premises.csv")
+
+    save_dir = "../llm_story_generation_results_v2/"
+
+    # the number of stories to be generated per prompt
+    n_gen = 3
+    
+    # To generate stories for all, set regen_ids to empty or None 
+    regen_ids = [0, 1, 2]
+
+    # min. story length
+    min_len = 400
+
+    generator.output_stories(premises, save_dir, llm, n_gen, regen_ids, min_len)
