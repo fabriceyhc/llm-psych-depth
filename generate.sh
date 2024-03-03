7b198e808dc23801534d51ea4c06d3ab87f77767
Fabrice Harel-Canada <fabricehc@pluslab-a6000.cs.ucla.edu>
2024-03-03 09:54:21 -0800

    adding retry count analysis

--- generate.sh
+++ generate.sh
@@ -1,12 +1,12 @@
-# python -m story_generation.generate \
-#           generator_args.model_name_or_path="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ" \
-#           generation_args.strategy="writer_profile" \
-#           cuda_visible_devices=\'0,1\'
+python -m story_generation.generate \
+          generator_args.model_name_or_path="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ" \
+          generation_args.strategy="writer_profile" \
+          cuda_visible_devices=\'2,3\'
 
-# python -m story_generation.generate \
-#           generator_args.model_name_or_path="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ" \
-#           generation_args.strategy="plan_write" \
-#           cuda_visible_devices=\'0,1\'
+python -m story_generation.generate \
+          generator_args.model_name_or_path="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ" \
+          generation_args.strategy="plan_write" \
+          cuda_visible_devices=\'4,5\'
 
 python -m story_generation.generate \
           generator_args.model_name_or_path="TheBloke/Llama-2-7B-chat-GPTQ" \
@@ -28,25 +28,25 @@ python -m story_generation.generate \
           generation_args.strategy="plan_write" \
           cuda_visible_devices=\'6,7\'
 
-# python -m story_generation.generate \
-#           generator_args.model_name_or_path="TheBloke/Llama-2-70B-chat-GPTQ" \
-#           generation_args.strategy="writer_profile" \
-#           cuda_visible_devices=\'0,1\'
+python -m story_generation.generate \
+          generator_args.model_name_or_path="TheBloke/Llama-2-70B-chat-GPTQ" \
+          generation_args.strategy="writer_profile" \
+          cuda_visible_devices=\'0,1\'
 
-# python -m story_generation.generate \
-#           generator_args.model_name_or_path="TheBloke/Llama-2-70B-chat-GPTQ" \
-#           generation_args.strategy="plan_write" \
-#           cuda_visible_devices=\'6,7\'
+python -m story_generation.generate \
+          generator_args.model_name_or_path="TheBloke/Llama-2-70B-chat-GPTQ" \
+          generation_args.strategy="plan_write" \
+          cuda_visible_devices=\'0,1\'
 
 # python -m story_generation.generate \
 #           generator_args.model_name_or_path="lmsys/vicuna-33b-v1.3" \
 #           generation_args.strategy="writer_profile" \
 #           cuda_visible_devices=\'0,1,2,3\'
 
-# python -m story_generation.generate \
-#           generator_args.model_name_or_path="lmsys/vicuna-33b-v1.3" \
-#           generation_args.strategy="plan_write" \
-#           cuda_visible_devices=\'4,5,2,3\'
+python -m story_generation.generate \
+          generator_args.model_name_or_path="lmsys/vicuna-33b-v1.3" \
+          generation_args.strategy="plan_write" \
+          cuda_visible_devices=\'2,3,4,5\'
 
 # python -m story_generation.generate \
 #           generator_args.model_name_or_path="gpt-4" \
