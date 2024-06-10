1060dda8727ec0aad9793b276ea0aa017f8c12bf
Fabrice Harel-Canada <fabricehc@pluslab01-a6000.cs.ucla.edu>
2024-06-10 01:08:02 -0700

    adding visualization for t-tests

--- story_eval/visualize.py
+++ story_eval/visualize.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import itertools
+from scipy.stats import ttest_ind
 
 # Function to add a radar chart with filled variance
 def add_radar_chart_filled_variance(ax, angles, data_row, label, color, variance_fraction=0.2, line_width=3):
@@ -22,7 +24,7 @@ def plot_spider(df, models, title, sort_order, colors, save_path=None):
     filtered_data = df[df["model_short"].isin(models)]
 
     # Remove 'participant_id' and 'story_id' from the aggregation
-    filtered_data_no_ids = filtered_data.drop(columns=['participant_id', 'story_id', 'model_full', 'strategy'])
+    filtered_data_no_ids = filtered_data.drop(columns=['participant_id', 'story_id', 'model_short', 'strategy'])
 
     # Re-aggregate the data without participant and story IDs
     aggregated_filtered_data_no_ids = filtered_data_no_ids.groupby('model_short').agg(['mean', 'var'])
@@ -122,7 +124,7 @@ def plot_cdf(df, models, title, sort_order, colors, smooth=True, save_path=None)
 def plot_component_corrs(df, title=None, save_path=None):
     components = [
         'authenticity_score', 'empathy_score', 'engagement_score', 
-        'emotion_provoking_score', 'narrative_complexity_score', 'human_likeness_score']
+        'emotion_provoking_score', 'narrative_complexity_score']
     
     labels = ["AUTH", "EMP", "ENG", "PROV", "NCOM", "HUM"]
     correlation_matrix = df[components].corr()
@@ -145,13 +147,96 @@ def plot_component_corrs(df, title=None, save_path=None):
     else:
         plt.show()
 
+def display_pairwise_t_tests(df, title=None, save_path=None):
+
+    col = "model_short"
+    components = [
+        'authenticity_score', 'empathy_score', 'engagement_score', 
+        'emotion_provoking_score', 'narrative_complexity_score']
+    
+    unique_vals = df[col].sort_values().unique()
+    t_test_results = {}
+    for component in components:
+        for val1, val2 in itertools.combinations(unique_vals, 2):
+            # Sort model names to ensure consistency
+            model1, model2 = sorted([val1, val2])
+            data_i = df[df[col] == model1][component]
+            data_j = df[df[col] == model2][component]
+
+            # Perform t-test between the two models
+            t_stat, p_val = ttest_ind(data_i, data_j, nan_policy='omit')
+
+            # Append the result
+            t_test_results[(component,model1,model2)]= {
+                'component': component,
+                f'{col}_1': model1,
+                f'{col}_2': model2,
+                't_stat': t_stat,
+                'p_value': p_val
+            }
+
+    df = pd.DataFrame.from_dict(t_test_results, orient='index')
+
+    # Mapping for readable labels
+    component_labels = {
+        'authenticity_score': 'AUTH',
+        'emotion_provoking_score': 'PROV',
+        'empathy_score': 'EMP',
+        'engagement_score': 'ENG',
+        'narrative_complexity_score': 'NCOM'
+    }
+    
+    # Creating a new column with combined model names
+    df['model_comparison'] = df['model_short_1'] + " vs. " + df['model_short_2']
+
+    # Creating pivot tables for t-statistics and p-values with combined model names
+    pivot_table_t = df.pivot_table(index='model_comparison', columns='component', values='t_stat')
+    pivot_table_p = df.pivot_table(index='model_comparison', columns='component', values='p_value')
+
+    # Renaming the columns in the pivot tables
+    pivot_table_t.rename(columns=component_labels, inplace=True)
+    pivot_table_p.rename(columns=component_labels, inplace=True)
+
+    # Plot heatmap for t-statistics (indicating direction and magnitude of change)
+    plt.figure(figsize=(15, 10))
+    
+    # Create a custom annotator function to show p-values
+    def annot_func(data):
+        return data.map(lambda v: f"{v:.2f}")
+
+    # Annotate heatmap with p-values
+    annotations = annot_func(pivot_table_p)
+    ax = sns.heatmap(
+        pivot_table_t, 
+        annot=annotations, 
+        fmt="", 
+        cmap='coolwarm_r', 
+        center=0, 
+        cbar_kws={'label': 't-statistic'}, 
+        annot_kws={"size": 14},
+        # vmin=-1, 
+        # vmax=1
+    )
+    ax.figure.axes[-1].set_ylabel('T-Statistic', size=16)  
+
+    # Title and labels
+    plt.title(title, pad=20, fontsize=20)
+    plt.xticks(rotation=0, fontsize=16)
+    plt.yticks(rotation=0, fontsize=16)
+    plt.xlabel('PSD Component', fontsize=18)
+    plt.ylabel('Author Comparisons', fontsize=18)
+
+    if save_path:
+        plt.savefig(save_path, bbox_inches='tight')
+    else:
+        plt.show()
 
 if __name__ == "__main__":
 
     import os
 
-    scores_path = "./human_study/data/processed/human_annotations.csv"
-    human_ratings_df = pd.read_csv(scores_path)# .drop(columns=["human_likeness_score"])
+    scores_path = "./human_study/data/processed/round1/human_annotations.csv"
+    human_ratings_df = pd.read_csv(scores_path).drop(columns=["human_likeness_score"])
 
     sort_order = {
         "Llama-2-7B" :        0, 
@@ -174,37 +259,43 @@ if __name__ == "__main__":
 
     save_path = "./story_eval/imgs/"
 
+    display_pairwise_t_tests(
+        df=human_ratings_df,
+        title='Authorship Comparisons',
+        save_path=os.path.join(save_path, "pairwise_t-test_p-values.png")
+    )
+
     plot_component_corrs(
         df=human_ratings_df,
         title='',
         save_path=os.path.join(save_path, "component_corrs.png")
     )
 
-    # Spider Plots
-    plot_spider(
-        df=human_ratings_df, 
-        models=gpt4_vs_humans, 
-        title="", # Mean Psychological Depth Scores
-        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_humans.png"),
-        sort_order=sort_order,
-        colors=colors
-    )
-    plot_spider(
-        df=human_ratings_df, 
-        models=gpt4_vs_llms, 
-        title="", # Mean Psychological Depth Scores
-        save_path=os.path.join(save_path, "mean_scores_gpt4_vs_llms.png"),
-        sort_order=sort_order,
-        colors=colors
-    )
-    plot_spider(
-        df=human_ratings_df, 
-        models=all_models, 
-        title="", # Mean Psychological Depth Scores
-        save_path=os.path.join(save_path, "mean_scores_all.png"),
-        sort_order=sort_order,
-        colors=colors
-    )
+    # # Spider Plots
+    # plot_spider(
+    #     df=human_ratings_df, 
+    #     models=gpt4_vs_humans, 
+    #     title="", # Mean Psychological Depth Scores
+    #     save_path=os.path.join(save_path, "mean_scores_gpt4_vs_humans.png"),
+    #     sort_order=sort_order,
+    #     colors=colors
+    # )
+    # plot_spider(
+    #     df=human_ratings_df, 
+    #     models=gpt4_vs_llms, 
+    #     title="", # Mean Psychological Depth Scores
+    #     save_path=os.path.join(save_path, "mean_scores_gpt4_vs_llms.png"),
+    #     sort_order=sort_order,
+    #     colors=colors
+    # )
+    # plot_spider(
+    #     df=human_ratings_df, 
+    #     models=all_models, 
+    #     title="", # Mean Psychological Depth Scores
+    #     save_path=os.path.join(save_path, "mean_scores_all.png"),
+    #     sort_order=sort_order,
+    #     colors=colors
+    # )
 
     # CDF plots
     plot_cdf(
